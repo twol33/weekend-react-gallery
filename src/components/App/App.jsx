8e1bdbddc5f0c69bbb5f6b51849119126a05ee10
Tou Xiong <tlxiong.prime@outlook.com>
2021-09-17 21:35:52 -0500

    created GET and POST or app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    getImages();
+  })
+
+  const [imageList, setImageList] = useState([]);
+
+  const getImages = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery',
+    }).then((reponse) => {
+      console.log(reponse);
+      setImageList();
+    }).catch((error) => {
+      console.log(error);
+      alert('Could Not GET /gallery')
+    })
+  }
+
+  const postImages = () => {
+    axios({
+      method: 'POST',
+      url: '/gallery',
+      data: [ path, description ]
+    }).then((respond) => {
+      console.log(respond);
+      getImages();
+    }).catch((error) => {
+      console.log(error);
+      alert('Could Not POST /gallery')
+    })
+  }
+
     return (
       <div className="App">
         <header className="App-header">
