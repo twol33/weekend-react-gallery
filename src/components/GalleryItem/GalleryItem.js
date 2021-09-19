c36fdfac436809697baaea653c155eb0dda6d31a
Tou Xiong <tlxiong.prime@outlook.com>
2021-09-18 23:23:40 -0500

    attempting to create an onClick to swap image with description

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,11 +1,10 @@
-import GalleryList from "../GalleryList/GalleryList";
 
 function GalleryItem ( props ){
 
     return(
         <>
         {props.list.map( image => (
-            <p><button key={image.id}>Like Button</button></p>
+            <p><button key={image.id} onClick={image.description}><img src={image.path}/></button></p>
         ))}
         </>
 
--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -3,7 +3,7 @@ function GalleryList( props ) {
     return(
     <>
             {props.list.map( image => (
-                <p key={image.id}><img src={image.path}/><button>Like</button><span>0</span></p>
+                <p key={image.id}><img src={image.path} onClick={image.description}/><button>Like This</button><span>0</span></p>
             ))}
     </>
     );
