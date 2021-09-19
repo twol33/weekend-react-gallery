3d8ac64039592030bad34041b18103de1816abc1
Tou Xiong <tlxiong.prime@outlook.com>
2021-09-18 21:57:23 -0500

    added sample photos from google with sample descriptions

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,12 @@
-const galleryItems = [ (id, title, description, path),
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+const galleryItems = [
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/Flagfootballut.jpg', description: 'Hobby I have played for 10+ years', like: 0},
+    { id: 3, path: 'images/IS-Largemouth-Bass.jpg', description: 'Rippin Lips!', like: 0},
+    { id: 4, path: 'images/package-1.jpg', description: 'Home from Home', like: 0},
+    { id: 5, path: 'images/Snowboard_pow.jpg', description: 'Loving MN more', like: 0},
+    { id: 6, path: 'images/Super73.jpg', description: 'Always down for the future', like: 0},
+    { id: 2, path: 'images/tiny-house.jpg', description: 'Living the Life', like: 0}
+
 ];
 
 module.exports = galleryItems;
